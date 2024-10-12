4bc9f7049b871b1591d615898f2dc3d42ef21f03
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-10-12 21:09:37 +0500

    Delete mileston 1-2 directory

--- mileston 1-2/script.ts
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementById("toggle-button") as HTMLButtonElement;
-const skills = document.getElementById("skills") as HTMLElement;
-toggleButton.addEventListener('click', () => {
-    if(skills.style.display === 'none') {
-        skills.style.display = 'block'
-    }else{
-        skills.style.display = 'none'
-    }
-});
